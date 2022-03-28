890c6ca06071c2beaef8717e45c3d7fb74cf621e
Rufus Roby Mathew <30586379+heyitsrufus@users.noreply.github.com>
2022-03-27 21:11:54 -0400

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,4 @@
-
+window.addEventListener('load', function () {
+  console.log('This function is executed once the page is fully loaded. This is because you are not sanitizing your inputs.');
+  alert("Hello! I am an alert box!! This is a proof of concept that your website is vulnerable to XSS.");
+});
